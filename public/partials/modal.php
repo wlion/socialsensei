8120b8bca562ad5ab925d9fdd38d5011e509fb64
leecjaster <leecjaster@gmail.com>
2023-11-10 11:58:21 -0600

    modal updates - pre ajax call

---
+++ public/partials/modal.php
@@ -0,0 +1,18 @@
+<div id="social-sensei-modal" class="modal modal--lg">
+    <div class="modal__dialog">
+        <div class="modal__content">
+            <span class="modal__close" data-modal-close></span>
+            <header class="modal__header">
+                <h2 class="modal__title">Share this page on <span class="modal__social"></span></h2>
+                <h5 class="modal__instructions">Copy and paste the following AI generated content and publish to <span class="modal__social"></span></h2>
+            </header>
+            <section class="modal__body">
+                <p>Nostrud ad veniam nulla aute est velit. Esse esse cupidatat amet velit id elit consequat minim ullamco mollit. Excepteur ea laboris adipisicing aliqua proident occaecat do.</p>
+            </section>
+            <footer class="modal__footer u--text-align-right">
+                <a href="#modal-extra-small" class="btn" >Re-generate</a>
+                <a href="#modal-extra-small" class="btn" data-modal-close>Close</a>
+            </footer>
+        </div>
+    </div>
+</div>
